e7688c1b46e3658859fc699fe723d35e01711898
nico martinez <divnicokovacdiv@gmail.com>
2023-03-20 21:25:36 -0300

    Location 2

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import React  from 'react';
+import * as React  from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from "react-native-maps";
 
-
 export default function App() {
+
+
+
+  const [ origin, setOrigin ] = React.useState({
+    latitude: -33.43697412635504, 
+    longitude: -70.63442101351201
+});
+
+
+
+
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}/>      
+      <MapView 
+        style={styles.map}
+        initialRegion={{
+          latitude: origin.latitude,
+          longitude: origin.longitude,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04,
+        }}
+        />      
     </View>
   );
 }
